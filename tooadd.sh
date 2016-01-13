bd1f01ce8523e04c4bbca8097e00fa260a8b1c18
Hyeok_Oh <oh4851@gmail.com>
2016-01-13 10:04:22 +0900

    modify tooadd.sh
    - `pwd`/{filename} -> `realpath {filename}`
    - add if/else condition with $option

--- tooadd.sh
+++ tooadd.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # check file already exists in manage_list
-filename=`pwd`'/'$1
+filename=`realpath $1`
 result=`sed -n "\,$filename,p" ~/TOORM/manage_list.dat`
 
 if [ -z $result ]; then
     echo $'\n''[TOORM: new file !!!]'
     echo -n '[TOORM: manage this file? (y/n)] '
     read option
+    if [ $option = 'y' ]; then
+        echo "yes"
+    else
+        echo "no"
+    fi
 fi
